d6923fa480a8e3b876c78bc555274886499b82d7
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-01-05 20:47:42 +0100

    convert to streamlit app

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
+import streamlit as st
+import time
+import threading
 from src.ai import AI
 from src.audio import record_audio, save_as_mp3
+from src.ui import space
 
+# set page title
+st.markdown("<h1 style='text-align: center; color: black;'>AI Voice Assistant</h1>",
+            unsafe_allow_html=True)
+
+space(3)
+def speak_in_thread(response):
+    ai.speak(response)
+
+# create an instance of the AI class
 ai = AI()
-recording = record_audio(5)
-save_as_mp3(recording, 44100, "audiofiles/input.mp3")
-transcript = ai.listen("audiofiles/input.mp3")
-response = ai.think(transcript)
-ai.speak(response)
-print(response)
+
+# Initialize a session state for message history if it doesn't exist
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# record and save audio
+if st.button("Record"):
+    recording = record_audio(5)
+    save_as_mp3(recording, 44100, "audiofiles/input.mp3")
+
+    # listen
+    transcript = ai.listen("audiofiles/input.mp3")
+    with st.chat_message("user"):
+        st.markdown(transcript)
+    st.session_state.messages.append({"role": "user", "content": transcript})
+
+    # Get response using the updated history
+    response = ai.think(transcript, st.session_state.messages)
+    speak_thread = threading.Thread(target=speak_in_thread, args=(response,))
+    speak_thread.start()
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for chunck in response.split():
+            full_response += chunck + " "
+            message_placeholder.markdown(full_response +  "▌")
+            time.sleep(0.15)
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
--- src/ai.py
+++ src/ai.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
+import streamlit as st
 import os
 
 class AI:
+    
     def __init__(self, api_key=os.environ.get("OPENAI_API_KEY")):
         self.client = OpenAI(api_key=api_key)
 
@@ -12,21 +14,36 @@ class AI:
                 file=file
             )
         return transcript.text
+    
+    def listen_in_english(self, audio_file, model="whisper-1"):
+        audio_file= open(audio_file, "rb")
+        transcript = self.client.audio.translations.create(
+            model="whisper-1", 
+            file=audio_file
+            )
+        return transcript.text
 
-    def think(self, input_text, model="gpt-3.5-turbo"):
-        completion = self.client.chat.completions.create(
-            model=model,
-            messages=[
-                {"role": "user", "content": input_text},
-            ]
-        )
+    def think(self, input_text, message_history, model="gpt-3.5-turbo"):
+        messages = message_history + [{"role": "assistant", "content": """You're an AI tool connected to a
+                                speech-to-text and text-to-speech model used to make people think
+                                who ask you questions. You have to behave as if you were face-to-face
+                                with them, in your own voice, even if you don't look phyisical. Your
+                                role is to ask pertinent questions and create interesting philosophical
+                                discussions with the people who talk to you, based on the topics they choose.
+                                To create interesting discussions, you take a keen interest in the people
+                                who talk to you, always asking them questions about who they are, what they
+                                do and what they think. When you express yourself, don't say you're an
+                                artificial intelligence, just a normal person who's happy to be here."""}]
+        messages = messages + [{"role": "user", "content": input_text}]
+        completion = self.client.chat.completions.create(model=model, messages=messages)
         return completion.choices[0].message.content
 
-    def speak(self, input_text, path="audiofiles/speech.mp3", voice="alloy", model="tts-1"):
+    def speak(self, input, path="audiofiles/speech.mp3", voice="nova", model="tts-1", speed=1.2):
         response = self.client.audio.speech.create(
             model=model,
             voice=voice,
-            input=input_text
+            input=input,
+            speed=speed
         )
         response.stream_to_file(path)
         os.system(f"afplay {path}")
